bf25f927c6e4784738388da4e204c19f98cc64dd
GustavoZom <168488714+GustavoZom@users.noreply.github.com>
2026-03-12 00:10:03 +0000

    aa

---
+++ Aula02/src/main/java/edu/unipam/aula/EmailService.java
@@ -0,0 +1,9 @@
+package edu.unipam.aula.services;
+
+public class EmailService {
+    
+    public void enviar(String mensagem){
+        System.out.println("Enviando email: " + mensagem);
+
+    }
+}
---
+++ Aula02/src/test/java/edu/unipam/aula/AulaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unipam.aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
